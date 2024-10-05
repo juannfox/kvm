d8cc2c14a56ad60c07c65d8c2a9cf273511271d7
Juan Fox <juannfox@hotmail.com>
2024-10-05 15:51:05 -0300

    Add basic typer

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import re
 from dataclasses import dataclass
 import requests
 from os import getenv
+import typer
 
 
 DEFAULT_VERSION_FETCH_URL = "https://cdn.dl.k8s.io/release/stable.txt"
@@ -83,15 +84,34 @@ def generate_release_url(spec: ReleaseSpec) -> str:
 LOG_LEVEL = "DEBUG" if getenv("DEBUG") == "1" else "INFO"
 log.basicConfig(level=LOG_LEVEL)
 
-latest_version = fetch_latest_version()
-version_spec = ReleaseSpec(version=latest_version)
-release_get_url = generate_release_url(version_spec)
+app = typer.Typer()
 
-log.debug(f"Downloading kubectl release from: {release_get_url}.")
-download_response = requests.get(
-        release_get_url, stream=True, timeout=DEFAULT_HTTP_TIMEOUT)
-with open("kubectl", "wb") as f:
-    for chunk in download_response.iter_content(chunk_size=1024):
-        if chunk:
-            f.write(chunk)
-    f.close()
+
+@app.command()
+def latest():
+    latest_version = fetch_latest_version()
+    ReleaseSpec.validate_version(latest_version)
+    log.info(f"Latest kubectl version: {latest_version}.")
+
+
+@app.command()
+def install(version: str = None):
+    if version is None:
+        version = fetch_latest_version()
+    version_spec = ReleaseSpec(version=version)
+    release_get_url = generate_release_url(version_spec)
+
+    log.debug(f"Downloading kubectl release from: {release_get_url}.")
+    download_response = requests.get(
+            release_get_url, stream=True, timeout=DEFAULT_HTTP_TIMEOUT)
+    with open("kubectl", "wb") as f:
+        for chunk in download_response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+        f.close()
+
+    log.info(f"Downloaded kubectl {version}.")
+
+
+if __name__ == "__main__":
+    app()
